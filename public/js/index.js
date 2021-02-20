ae6f6d23126c5824ab0343f6b83dc40d48466042
JEONMINJU <junielle.jeon@gmail.com>
2021-02-21 00:26:50 +0900

    ...

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,8 @@
 /****************** 전역설정 *******************/
 var idx = 0; // Home, Skills, Project (0,1,2)
 
-var path = document.querySelector('.path');
-var length = path.getTotalLength();
+/* var path = document.querySelector('.path');
+var length = path.getTotalLength(); */
 
 /* var typingBool = false; 
 var typingIdx = 0; 
@@ -19,9 +19,7 @@ $(".navi-wrap span").click(onNaviAct);
 
 /* 모달 */
 $(".header-wrapper .about-wrap").click(onModalShow);
-$(".modal-wrap .modal-btn").click(onModalHide);
-
-
+$(".modal-wrapper .close-btn").click(onModalHide);
 
 
 /****************** 이벤트콜백 *******************/
@@ -84,9 +82,47 @@ function onNaviAct() {
 	$(".navi-wrap span").eq(idx).addClass('active');
 }
 
- function onFirstLineShow() {
- $('.main-wrapper .first-line').addClass('active');
-}
-
-
 /****************** 사용자함수 *******************/
+
+var typingBool = false; 
+var typingIdx=0; 
+var liIndex = 0;
+var liLength = $(".typing-txt>ul>li").length;
+
+// 타이핑될 텍스트를 가져온다 
+var typingTxt = $(".typing-txt>ul>li").eq(liIndex).text(); 
+typingTxt=typingTxt.split(""); // 한글자씩 자른다. 
+if(typingBool==false){ // 타이핑이 진행되지 않았다면 
+    typingBool=true; 
+    var tyInt = setInterval(typing,100); // 반복동작 
+} 
+     
+function typing(){ 
+  $(".typing ul li").removeClass("on");
+   $(".typing ul li").eq(liIndex).addClass("on");
+  if(typingIdx<typingTxt.length){ // 타이핑될 텍스트 길이만큼 반복 
+     $(".typing ul li").eq(liIndex).append(typingTxt[typingIdx]); // 한글자씩 이어준다. 
+     typingIdx++; 
+   } else{ if(liIndex<liLength-1){
+     //다음문장으로  가기위해 인덱스를 1증가
+       liIndex++; 
+     //다음문장을 타이핑하기위한 셋팅
+        typingIdx=0;
+        typingBool = false; 
+        typingTxt = $(".typing-txt>ul>li").eq(liIndex).text(); 
+       
+     //다음문장 타이핑전 1초 쉰다
+         clearInterval(tyInt);
+          //타이핑종료
+     
+         setTimeout(function(){
+           //1초후에 다시 타이핑 반복 시작
+           tyInt = setInterval(typing,100);
+         },1000);
+        } else if(liIndex==liLength-1){
+          
+         //마지막 문장까지 써지면 반복종료
+           clearInterval(tyInt);
+        }
+    } 
+}  
