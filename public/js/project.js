37b4a24f9dc9d70e640419afb4d9a96a9603e260
JEONMINJU <junielle.jeon@gmail.com>
2021-03-02 17:55:52 +0900

    ...

--- public/js/project.js
+++ public/js/project.js
@@ -40,9 +40,14 @@ function onSubClick() {
 
 /****************** 사용자함수 *******************/
 
-var swiper = new Swiper('.swiper-container', {
+
+var swiper = new Swiper('.cont-wrapper .swiper-container', {
 	slidesPerView: 1,
 	loop: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
